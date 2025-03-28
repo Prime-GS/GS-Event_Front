6b2534c5216dbac162a413906683d3c0bee108cf
Prime-GS <respokn@gmail.com>
2025-03-28 17:44:24 +0500

    Bug Fix: Category doesn't add and Update event button

--- src/sections/events/EventDetails.tsx
+++ src/sections/events/EventDetails.tsx
@@ -1,13 +1,24 @@
 import { IEvent } from '@/graphql/types/events'
+import { useStores } from '@/stores/hooks'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 interface IProps {
   event: IEvent
 }
 
 export function EventDetails({ event }: IProps) {
+  const { authStore } = useStores()
+
+  const creatorCheck = (authStore.user?.id === event.creatorId || authStore.user?.roles?.includes('admin')) ?? false
+
   return (
-    <div className='container'>
+    <div className='container position-relative'>
+      {creatorCheck && (
+        <Link to={`/events/edit/${event.id}`}>
+          <button className='btn btn-primary position-absolute end-0'>Редактировать</button>
+        </Link>
+      )}
       <h1 className='mb-0'>{event.title}</h1>
       <hr />
       <p> {event.description}</p>
@@ -15,16 +26,14 @@ export function EventDetails({ event }: IProps) {
         <strong>Дата начала:</strong> {moment(event.startedAt).format('MMMM D HH:mm')}
       </p>
       <p>
-        <strong>Подписавшиеся на ивент:</strong> {event.categories.length}
+        <strong>Подписавшиеся на ивент:</strong> {event.subscribers.length}
       </p>
 
       <hr className='mt-0' />
       <div className='mt-3'>
         {event.categories.length !== 0 && (
           <>
-            <p>
-              <strong>Категорий:</strong>
-            </p>
+            <strong>Категорий:</strong>
             <div className='d-flex flex-wrap gap-2 mb-3 bg-secondary p-2 rounded-3'>
               {event.categories.map((cat) => {
                 return (
@@ -42,7 +51,7 @@ export function EventDetails({ event }: IProps) {
         )}
         <p className='mt-3 w-100'>
           <strong>Создатель Ивента:</strong> {event.creator.username}
-          <button className='btn btn-primary ms-auto d-block'>Подписаться</button>
+          <button className='btn btn-primary position-absolute end-0'>Подписаться</button>
         </p>
       </div>
     </div>
